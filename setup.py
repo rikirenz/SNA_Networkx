dae86e4d15651497d1199082f5f6d81c53b6f485
Riccardo Candido <riccardocandido@gmail.com>
2017-01-16 13:13:41 +0100

    create graph from the monomodal matrix

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import os
+
+from setuptools import find_packages, setup
+
+
+setup(
+    install_requires=[
+        'matplotlib>=1.5.3',
+        'networkx>=1.11',
+    ]
+)
+
